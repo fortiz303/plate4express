7f875569e74a17b34dc9d0b5f66bc6ebfe932d4a
student <student@PATC21.local>
2019-05-08 12:38:19 -0700

    finished backend

--- app.js
+++ app.js
@@ -59,43 +59,54 @@ app.post('/createNote',(request,response)=> {
   });
 
   newNote.save((error)=>{
-    if(error){
-      console.log('Something happened with mongoose',error);
-      //Respond to front end if failed
-      response.sendStatus(500);
-    } else {
-      console.log("Saved mongoose document succesfully");
-      //Respond to front end if succesful
-      response.send({text: 'Everything good baby'})
-    }
+    responseState(error, response, 200);
   });
 });
 //A post handler for reading notes from the and sending them to the front end
 app.post('/readNotes',(request,response) => {
   //Searches the MongoDB database and gets all the notes
   toDoModel.find({}, (error, results) => {
-    if (error) {
-      // If there is an error, send to front end code 500
-      console.log('Something happened with Mongoose.', error);
-      response.sendStatus(500);
-    } else {
-    //Otherwise send to front end what we got from database
-      let dataToSend = {notes: results};
-      response.send(dataToSend);
-    }
+    responseState(error, response, {notes: results});
   });
 });
 
 app.post('/deleteNote',(request, response) =>{
   //Searches the mongodb by an id and deletes this document.
-  toDoModel.findByIdAndDelete(request.body._id,(error, results)=>{
-    if (error) {
-      //If there is an error, send to front end code 500
-      console.log('Something happened with mongoose', error);
-      response.sendStatus(500);
-    } else {
-      //Otherwise, send to front end the item we deleted that is stored in the variable results
-      response.send({deleted: results});
-    }
-  })
+  toDoModel.findByIdAndDelete(request.body._id, (error,results)=>{
+    responseState(error, response, {deleted: results})
+  });
 });
+
+app.post('/updateNode',(request,response) =>{
+
+  let.propertiesToUpdate = {
+    username: request.body.username,
+    title: request.body.title,
+    description: request.body.description,
+    priority: request.body.priority,
+    dueDate: request.body.dueDate,
+    status: request.body.status,
+    list: null
+  }
+
+  toDoModel.findByIdAndUpdate(request.body._id, propertiesToUpdate, (error,results) => {
+    responseState(error, response, {updated: results});
+  });
+});
+
+
+
+
+function responseState(error,response,send){
+  if(error){
+    console.log('Something happened with mongoose: ', error);
+    response.sendStatus(500);
+  }else{
+    if(typeof send == 'number'){
+      console.log('response:', send);
+      response.sendStatus(send)
+    } else if(typeof send == 'object'){
+      response.send(send)
+    }
+  }
+}
